451e951319a1a4d4fa4beb6c53a18aa02086371d
CrnogoracIvan <crrnii@yahoo.com>
2017-05-09 21:02:51 +0200

    All tasks completed

--- src/tasks/Task04.java
+++ src/tasks/Task04.java
@@ -1,33 +1,43 @@
 package tasks;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by Ivan Crnogorac on 5/8/2017.
  */
 public class Task04 {
 
+    public static int[] mergeArrays2 ( int[] foo, int[] bar){
+        int[] merged = new int[foo.length + bar.length];
+        System.arraycopy(foo, 0, merged, 0, foo.length);
+        System.arraycopy(bar, 0, merged, foo.length, bar.length);
+
+        Set <Integer> nodupes = new HashSet <Integer>();
+
+        for (int i = 0; i < merged.length; i++) {
+            nodupes.add(merged[i]);
+        }
+
+        int[] nodupesarray = new int[nodupes.size()];
+        int i = 0;
+        Iterator <Integer> it = nodupes.iterator();
+        while (it.hasNext()) {
+            nodupesarray[i] = it.next();
+            i++;
+        }
+
+        return nodupesarray;
+}
+
     public static void main(String[] args) {
+         int[] foo = {1, 2, 3, 5, 7};
+         int[] bar = {1, 4, 6, 7, 8};
 
-        int[] foo = {1, 2, 3, 5, 7};
-        int[] bar = {1, 4, 6, 7, 8};
-        int[] baz = new int[foo.length + bar.length];
-
-        System.arraycopy(foo, 0, baz, 0, foo.length);
-      //  System.arraycopy(bar, 0, baz, foo.length, bar.length);
-
-        for (int i: baz) {
-          // System.out.println(i);
-
-        }
-
-        for (int i = 0; i <foo.length ; i++) {
-            baz[i] = foo[i];
-        }
-
-        for (int i = 0; i <baz.length ; i++) {
-            for
-        }
-
+        for (int i: mergeArrays2(foo, bar)
+             ) {
+            System.out.println(i);
         }
     }
-
 }
